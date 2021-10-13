f316020c8db7354503003042fb5e80298acd9241
jlopato <92473426+jlopato@users.noreply.github.com>
2021-10-13 21:55:10 +0200

    test 05

--- app10.py
+++ app10.py
@@ -1,8 +1,15 @@
-# building a basic calculator
+# Lists:
+#            0      1       2       3     4
+friends = ["Ali","Ahmed","Walid","Tom","Sarah"]
 
-num1 = input("Enter num1: ")
-num2 = input("Enter num2: ")
+print(friends[1])
+print(friends[3])
+print(friends[-1])
 
-result = float (num1) + float(num2)
+print(friends[1:])
 
-print(result)
+print(friends[1:3])
+
+friends[2] = "Salah"
+
+print(friends)
