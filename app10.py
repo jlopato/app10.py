0572962fe1f8e956231ed24ac1443966d11c21d4
jlopato <92473426+jlopato@users.noreply.github.com>
2021-10-14 20:58:02 +0200

    test 07

--- app10.py
+++ app10.py
@@ -1,16 +1,44 @@
-# Lists:
-#            0      1       2       3     4
-friends = ["Ali","Ahmed","Walid","Tom","Sarah"]
+# Lists functions:
 
-print(friends[1])
-print(friends[3])
-print(friends[-1])
+luck_number = [4,8,15,16,23,42]
+#          0      1       2       3     4       5
+friends = ["Ali","Ali","Ahmed","Walid","Tom","Sarah"]
 
-print(friends[1:])
+friends1 = friends.copy()
 
-print(friends[1:3])
+print(friends1)
+lucky_numbers = [4,8,15,16,23,42]
 
-friends[2] = "Salah"
+friends = ["Ali",""Ali",Ahmed","Walid","Yosef","Mosa"]
 
-print(friends)
+friends.extend(lucky_number) This will append 2 lists together
 
+friends.append("Sarah") append new element to the list
+
+friends.insert(1,"Salah") insert element in index 1
+
+friends.remove("Mosa")
+
+friends.clear() getred of every single elements inside the list
+
+friends.pop() getred the last element of the list
+
+print(friends.index("Walid"))
+
+print(friends.index("Rovan")) not exist
+
+print(friends.count("Ali")) how many times element is exist.
+
+friends.sort() sort the list in ascending order alphapetical order
+
+lucky_numbers.sort()
+
+print(lucky_numbers)
+
+lucky_numbers.reverse() reverse the order
+
+print(lucky_numbers)
+
+friends2=friends.copy()
+
+print(fiends2)
