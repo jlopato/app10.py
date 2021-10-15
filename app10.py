02835463d56f06334dff3c6e9ecaeabe9aab8f65
jlopato <92473426+jlopato@users.noreply.github.com>
2021-10-15 20:26:35 +0200

    test 12

--- app10.py
+++ app10.py
@@ -1,14 +1,14 @@
-# If Statement :
+# If Statement & comparisons:
+
+def max_num(num1,num2,num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+print(max_num(180,150,156))
 
 
-is_male = True
-is_tall = False
 
-if is_male and is_tall:
-    print("you are a tall male")
-elif is_male and not(is_tall):
-    print("you are a short male")
-elif not(is_male) and is_tall:
-    print("you are a tall woman")
-else:
-    print("you are not tall nor male")
