502809dff8876dbbf40c9b7fe68756816e1f8cde
jlopato <92473426+jlopato@users.noreply.github.com>
2021-10-15 20:58:32 +0200

    test 14

--- app10.py
+++ app10.py
@@ -2,17 +2,17 @@
 
 # 3 variables
 
-num_1 = int(input("Enter num1 : "))
+num1 = int(input("Enter num1 : "))
 op = input("Enter OP: ")
-num_2 = int(input("Enter num2: "))
+num2 = int(input("Enter num2: "))
 
 if op == "+":
-    print(num_1 + num_2)
+    print(num1 + num2)
 elif op == "-":
-    print(num_1-num_2)
+    print(num1-num2)
 elif op == "/":
-    print(num_1/num_2)
+    print(num1/num2)
 elif op == "*":
-    print(num_1 * num_2)
+    print(num1 * num2)
 else:
     print("Invalid operator")
