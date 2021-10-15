95fe9acdb42baa40337629a9a3940b8b5e6a9858
jlopato <92473426+jlopato@users.noreply.github.com>
2021-10-15 22:02:40 +0200

    test 17

--- app10.py
+++ app10.py
@@ -1,14 +1,49 @@
-# Dictionaries :
+# Building A guesing Game:
 
-monthConversions = {
-    1: "January",
-    "Feb": "February",
-    "Mar": "March",
-    "Apr": "April",
-    "May": "May",
-    "Jun": "June",
-    "Jul": "July",
-    "Aug": "August",
-}
 
-print(monthConversions.get("Feb"))
+
+secret_word = "coffee"
+
+guess = ""
+
+
+
+while guess !=secret_word:
+
+    guess = input("Enter Guess: ")
+
+
+
+print("congrats, you won")
+
+
+
+--------
+
+secret_word = "coffee"
+
+guess = ""
+
+guess_counter = 0
+
+guess_limit = 3
+
+out_guess = False
+
+
+
+while guess !=secret_word and guess_counter < guess_limit:
+
+    guess_counter+=1
+
+    guess = input("Enter Guess: ")
+
+
+
+if guess_counter == guess_limit and guess != secret_word:
+
+    print("you lost")
+
+else:
+
+    print("congrats, you won")
