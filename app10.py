7cf6451868a06223ca4a00e553bc68b26cee6572
jlopato <92473426+jlopato@users.noreply.github.com>
2021-10-28 18:37:43 +0200

    test 24

--- app10.py
+++ app10.py
@@ -1,13 +1,6 @@
 # reading files:
-# open("employees.txt", "w")  w----> write
 
-# open("employees.txt", "a")  a----> append - append info to end of the file
-
-# open("employees.txt", "r+") r+---> read and write
-
-# open("employees.txt", "r")  r----> read
-
-Employees_file = open("Employees.txt", "r")
-print(Employees_file.readlines())
+Employees_file = open("Employees.txt", "a")
+Employees_file.write("\nJohn - Engineer")
 
 Employees_file.close()
