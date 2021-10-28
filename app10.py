4aeec4d2779c30bafc7c1f23d884e5a15546e094
jlopato <92473426+jlopato@users.noreply.github.com>
2021-10-28 18:43:04 +0200

    test 24

--- app10.py
+++ app10.py
@@ -1,6 +1,6 @@
 # reading files:
 
-Employees_file = open("Employees.txt", "a")
-Employees_file.write("\nJohn - Engineer")
+Employees_file = open("Employees.txt", "w")
+Employees_file.write("\nJohn - Engineer \nAli - Accountant \nAhmed - Accountant \nSarah - Receptionist")
 
 Employees_file.close()
