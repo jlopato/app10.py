48cb6054879ba785ebd4698f91240bba7830094d
jlopato <92473426+jlopato@users.noreply.github.com>
2021-10-29 16:24:49 +0200

    test 28

--- app10.py
+++ app10.py
@@ -1,1 +1,7 @@
 # Classes and objects:
+
+from car import car
+
+
+car1 = car("Toyota",2020,"white",3000)
+print(car1.price)
---
+++ car.py
@@ -0,0 +1,7 @@
+class car:
+    def __init__(self,brand,model,color,price):
+        self.color = color
+        self.brand = brand
+        self.model = model
+        self.price = price
+
