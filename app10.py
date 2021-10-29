d1cecb3a196e41d8a2b09f413540c0faf5344026
jlopato <92473426+jlopato@users.noreply.github.com>
2021-10-29 17:10:31 +0200

    test 29

--- app10.py
+++ app10.py
@@ -10,3 +10,12 @@ question = [
     Question(question_prompts[1],"c"),
     Question(question_prompts[2],"a"),
 ]
+def run_test(questions):
+    score = 0
+    for question in questions
+        answer = input(question_prompts)
+        if answer == question.answer
+            score+=1
+    print("you got" + str(score)+ "/" + str(len(questions))+ "correct")
+
+run_test(questions)
