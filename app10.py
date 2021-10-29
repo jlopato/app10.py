2dc27a2aa92ed339ac6b52c4312c614e564421de
jlopato <92473426+jlopato@users.noreply.github.com>
2021-10-29 17:17:52 +0200

    test 29

--- app10.py
+++ app10.py
@@ -1,5 +1,5 @@
 # Building a QCM :
-import Question from Question
+from Question import Question
 question_prompts = [
     "What is the color of apple? \na- Red\nb- White\nc- Black\n\n",
     "What is the color of banana? \na- Red\nb- White\nc- Black\n\n",
@@ -10,12 +10,12 @@ question = [
     Question(question_prompts[1],"c"),
     Question(question_prompts[2],"a"),
 ]
-def run_test(questions):
+def run_test(question):
     score = 0
-    for question in questions
-        answer = input(question_prompts)
-        if answer == question.answer
+    for question in question:
+        answer = input(question.prompt)
+        if answer == question.answer:
             score+=1
     print("you got" + str(score)+ "/" + str(len(questions))+ "correct")
 
-run_test(questions)
+run_test(question)
