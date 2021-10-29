ce708f2c9e394afdc6ff4503af21c83ac35cbfdf
jlopato <92473426+jlopato@users.noreply.github.com>
2021-10-29 17:21:25 +0200

    test 29

--- app10.py
+++ app10.py
@@ -5,17 +5,17 @@ question_prompts = [
     "What is the color of banana? \na- Red\nb- White\nc- Black\n\n",
     "What is the color of strawberry? \na- Red\nb- White\nc- Black\n\n",
 ]
-question = [
+questions = [
     Question(question_prompts[0],"a"),
     Question(question_prompts[1],"c"),
     Question(question_prompts[2],"a"),
 ]
-def run_test(question):
+def run_test(questions):
     score = 0
-    for question in question:
+    for question in questions:
         answer = input(question.prompt)
         if answer == question.answer:
             score+=1
-    print("you got" + str(score)+ "/" + str(len(questions))+ "correct")
+    print("you got " + str(score)+ "/" + str(len(questions))+ " correct")
 
-run_test(question)
+run_test(questions)
