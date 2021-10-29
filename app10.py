0ccb61a7d70206dba9c1c5ae9cfbcd0e5d5cc4b5
jlopato <92473426+jlopato@users.noreply.github.com>
2021-10-29 17:47:01 +0200

    test 30

--- app10.py
+++ app10.py
@@ -1,11 +1,11 @@
 # Object Fubnctions:
 
 from car import car
-
-
 car1 = car("Toyota",2020,"white",3000)
 #print(car1.price)
 car2 = car("Ferrari",2019,"yellow",50000)
 #print(car2.color)
 car3 = car("Nissan",2019,"black",1500)
 #print(car3.brand)
+
+print(car3.is_expensive())
