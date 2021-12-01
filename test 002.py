ed9cceab3b61c406d61e3fe92277258d03bfad38
jlopato <92473426+jlopato@users.noreply.github.com>
2021-12-01 21:42:01 +0100

    le wagon team

---
+++ test 002.py
@@ -0,0 +1,17 @@
+# Lists:
+#            0      1       2       3     4
+friends = ["Ali","Ahmed","Walid","Tom","Sarah"]
+
+print(friends[1])
+print(friends[3])
+print(friends[-1])
+
+print(friends[1:])
+
+print(friends[1:3])
+
+friends[2] = "Salah"
+
+print(friends)
+
+print(4532)
